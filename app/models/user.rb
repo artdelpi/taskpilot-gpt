299dafb12b1411cb76cc5fadc31aedc928ba7608
artdelpi <artdelpi@gmail.com>
2025-08-04 20:20:49 -0300

    fix: correct model setup for User and Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :project_assignments
+    has_many :projects, through: :project_assignments
     has_many :task_assignments, dependent: :destroy
     has_many :tasks, through: :task_assignments
     has_many :comments, dependent: :destroy
