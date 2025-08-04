e5f60cf011c23712a52b3bc4c1cfa9330cce2be7
artdelpi <artdelpi@gmail.com>
2025-08-03 23:11:02 -0300

    feat: initial web structure with routing and Devise integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     belongs_to :project
     has_many :task_assignments, dependent: :destroy
-    has_many :tasks, through :task_assignments
+    has_many :tasks, through: :task_assignments
     has_many :comments, dependent: :destroy
 end
